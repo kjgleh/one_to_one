61b5e049c273ef014b5363cfa5120623738bb78a
김정근 <gogoogjg@tripstore.kr>
2020-11-06 16:30:04 +0900

    initialize project

---
+++ src/test/kotlin/me/kjgleh/one_to_one/OneToOneApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.kjgleh.one_to_one
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OneToOneApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
